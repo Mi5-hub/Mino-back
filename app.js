85f1ffd029911430a46e4b03adbceaa2e6bc970b
mino <mino.randimbiarilanto@gmail.com>
2021-10-31 11:14:16 +0300

    index.html

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ require('./helpers/init_mongodb')
 const { verifyAccessToken } = require('./helpers/jwt_helper')
 require('./helpers/init_redis')
 
+
 const AuthRoute = require('./Routes/Auth.route')
 
 const app = express()
@@ -13,9 +14,14 @@ app.use(morgan('dev'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get('/', verifyAccessToken, async (req, res, next) => {
-  res.send('Hello from express.')
-})
+// app.get('/', verifyAccessToken, async (req, res, next) => {
+//   res.send('Hello from express.')
+// });
+
+app.get('/',function(req,res){
+  res.sendFile('./index.html');
+
+});
 
 app.use('/auth', AuthRoute)
 
