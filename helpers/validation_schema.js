7456131a0f235e3e371641b62355515c32e66b4a
mino <mino.randimbiarilanto@gmail.com>
2021-10-31 16:28:48 +0300

    save

--- helpers/validation_schema.js
+++ helpers/validation_schema.js
@@ -3,6 +3,9 @@ const Joi = require('@hapi/joi')
 const authSchema = Joi.object({
   email: Joi.string().email().lowercase().required(),
   firstname: Joi.string().min(2).lowercase().required(),
+  lastname: Joi.string().min(2).lowercase(),
+  date_naissance: Joi.string().min(2).lowercase(),
+  sexe: Joi.string().min(2).lowercase(),
   password: Joi.string().min(2).required()
 })
 
